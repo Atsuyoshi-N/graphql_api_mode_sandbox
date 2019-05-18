590246244591f2db01bd1b868f004cde40f954f3
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2019-05-19 00:35:24 +0900

    setup tslint

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -1,13 +1,15 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// Run this example by adding
+// <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import * as PropTypes from 'prop-types'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as PropTypes from 'prop-types';
 
 interface Props {
-  name?: string,
+  name?: string;
 }
 
 const Hello: React.FC<Props> = (props) => {
@@ -16,11 +18,11 @@ const Hello: React.FC<Props> = (props) => {
   );
 };
 
-Hello.defaultProps = { name: 'Atsuyoshi-N' }
+Hello.defaultProps = { name: 'Atsuyoshi-N' };
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello />,
     document.body.appendChild(document.createElement('div')),
-  )
-})
+  );
+});
